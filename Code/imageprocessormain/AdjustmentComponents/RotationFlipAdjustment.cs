b56accd59b3498473754ebb93829bec2662aa142
MikePere <mgperez@my.milligan.edu>
2019-01-25 09:32:22 -0500

    Create RotationFlipAdjustment.cs

---
+++ Code/imageprocessormain/AdjustmentComponents/RotationFlipAdjustment.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ImageProcessorMain.AdjustmentComponents
+{
+    internal class  RotationFlipAdjustment : IAdjustment
+{
+
+    
+}
